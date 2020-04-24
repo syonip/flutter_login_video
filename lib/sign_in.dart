dc4a36b0f13343d267a71af4b2b7ef5bf2abee55
Johnny <syonip@gmail.com>
2020-04-24 19:50:13 +0300

    sign in page

---
+++ lib/sign_in.dart
@@ -0,0 +1,158 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:video_player/video_player.dart';
+
+class SignIn extends StatefulWidget {
+  SignIn({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  VideoPlayerController _controller;
+  bool _visible = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    _controller = VideoPlayerController.asset("assets/videos/waves.mp4");
+    _controller.initialize().then((_) {
+      _controller.setLooping(true);
+      Timer(Duration(milliseconds: 100), () {
+        setState(() {
+          _controller.play();
+          _visible = true;
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_controller != null) {
+      _controller.dispose();
+      _controller = null;
+    }
+  }
+
+  _getVideoBackground() {
+    return AnimatedOpacity(
+      opacity: _visible ? 1.0 : 0.0,
+      duration: Duration(milliseconds: 1000),
+      child: SizedBox.expand(
+        child: FittedBox(
+          fit: BoxFit.fill,
+          child: SizedBox(
+            width: _controller.value.size?.width ?? 0,
+            height: _controller.value.size?.height ?? 0,
+            child: VideoPlayer(_controller),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _getBackgroundColor() {
+return Container(
+              decoration: new BoxDecoration(
+                color: Colors.blue.withAlpha(120),
+              ),
+            );
+  }
+
+  _getLoginButtons() {
+    
+  }
+
+  _getContent() {
+    return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
+                ),
+                Image(
+                  image: AssetImage("assets/images/white-logo.png"),
+                  width: 150.0,
+                ),
+                Text(
+                  "WaveSpy",
+                  style: TextStyle(color: Colors.white, fontSize: 40),
+                ),
+                Container(
+                  margin:
+                      const EdgeInsets.only(left: 30.0, right: 30.0, top: 40.0),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "View and share videos of current ocean conditions.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  width: double.infinity,
+                  child: FlatButton(
+                    color: Colors.white,
+                    padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                    child: const Text('Sign Up with Email'),
+                    onPressed: () async {},
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+                  width: double.infinity,
+                  child: FlatButton(
+                    color: Colors.blueAccent,
+                    padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                    child: const Text(
+                      'Log back in',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {},
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                      bottom: 16, top: 20, left: 20, right: 20),
+                  width: double.infinity,
+                  child: FlatButton(
+                    child: const Text(
+                      'Later...',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            _getVideoBackground(),
+            _getBackgroundColor(),
+            _getContent(),
+            _getContent(),
+          ],
+        ),
+      ),
+    );
+  }
+}
